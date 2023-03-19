7be37552df8e7ad9ad7d3cd140bbf3c3d8095ebf
Jonas Majauskas <jonasmajauskas1@gmail.com>
2023-03-19 19:29:31 -0400

    search any song

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import SearchBar from './components/SearchBar';
+import Gallery from'./components/Gallery';
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for music!')
+  let [data, setData] = useState([])
+  
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  useEffect(() => {
+      if(search) {
+      const fetchData = async () => {
+        document.title = `${search} Music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        if(resData.results.length > 0){
+          setData(resData.results)
+        } else {
+          setMessage('Not Found')
+        }
+      }
+      fetchData() //run the function
+    }
+  }, [search]) //dependency array reminding our 'useEffect' to only re-fire if the value of search is updated.
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
+
+  return (
+    <div className="App">
+      <SearchBar handleSearch = {handleSearch}/>
+      {message}
+      <Gallery data={data} />
+    </div>
+  );
+}
+
+export default App;
